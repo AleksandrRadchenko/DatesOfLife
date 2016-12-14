a63d2ef1f323057fdaf89816d2fc9d368d2ef1ed
partner.ar@gmail.com <fire1Stone>
2016-12-14 09:15:04 +0300

    Created MainActivity class and layout

---
+++ app/src/main/java/com/alsturm/datesoflife/MainActivity.java
@@ -0,0 +1,17 @@
+package com.alsturm.datesoflife;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ara on 14.12.2016.
+ */
+
+public class MainActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
